075e3db7dd85f0f412f5150260a06d30bf2895ea
Bruno Almeida <Bruno@brunos-air.identification7.com>
2016-07-15 13:43:52 -0400

    Create BeaconRangingService class

---
+++ CossetteNavigation/app/src/main/java/com/example/cossettenavigation/BeaconRangingService.java
@@ -0,0 +1,20 @@
+package com.example.cossettenavigation;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by Bruno on 2016-07-15.
+ */
+public class BeaconRangingService extends IntentService {
+
+    public BeaconRangingService() {
+        super("BeaconRangingService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+
+}
