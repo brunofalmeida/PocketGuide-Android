765992565b0fca7bc1a9c607b82e54a08386cf66
Bruno Almeida <Bruno@brunos-air.identification7.com>
2016-07-15 14:19:27 -0400

    Get Region data from BeaconRangingService intent

--- CossetteNavigation/app/src/main/java/com/example/cossettenavigation/BeaconRangingService.java
+++ CossetteNavigation/app/src/main/java/com/example/cossettenavigation/BeaconRangingService.java
@@ -2,6 +2,9 @@ package com.example.cossettenavigation;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
+
+import com.estimote.sdk.Region;
 
 /**
  * A service to manage beacon ranging (occurs at 1 second intervals).
@@ -10,13 +13,21 @@ import android.content.Intent;
  */
 public class BeaconRangingService extends IntentService {
 
+    private static final String TAG = "BeaconRangingService";
+
+    public static final String INTENT_KEY_REGION = "region";
+
     public BeaconRangingService() {
         super("BeaconRangingService");
     }
 
+    /**
+     * Service Intent handler. Expects a Region object.
+     */
     @Override
     protected void onHandleIntent(Intent intent) {
-
+        Region region = (Region) intent.getParcelableExtra(INTENT_KEY_REGION);
+        Log.v(TAG, region.toString());
     }
 
 }
