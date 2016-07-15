a0d848aa5c96dcc04a3c49904bdf03e953201de2
Bruno Almeida <Bruno@brunos-air.identification7.com>
2016-07-15 13:49:37 -0400

    Declare BeaconRangingService in manifest

--- CossetteNavigation/app/src/main/java/com/example/cossettenavigation/BeaconRangingService.java
+++ CossetteNavigation/app/src/main/java/com/example/cossettenavigation/BeaconRangingService.java
@@ -4,6 +4,8 @@ import android.app.IntentService;
 import android.content.Intent;
 
 /**
+ * A service to manage beacon ranging (occurs at 1 second intervals).
+ *
  * Created by Bruno on 2016-07-15.
  */
 public class BeaconRangingService extends IntentService {
