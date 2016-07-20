838cd2c75093e98377a892598c7ce47e18b7993a
Bruno Almeida <Bruno@brunos-air.identification7.com>
2016-07-20 12:43:41 -0400

    Implement basic anchor beacon methods and sample data

--- CossetteNavigation/app/src/main/java/com/example/cossettenavigation/Map.java
+++ CossetteNavigation/app/src/main/java/com/example/cossettenavigation/Map.java
@@ -1,5 +1,7 @@
 package com.example.cossettenavigation;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -11,6 +13,54 @@ import java.util.UUID;
  * Created by Bruno on 2016-07-20.
  */
 public class Map {
+
+    private static List<Beacon> anchorBeacons = new ArrayList<>();
+
+    private static final String DEFAULT_WHITE_UUID = "B9407F30-F5F8-466E-AFF9-25556B57FE6D";
+
+    /**
+     * Adds a beacon to the set of anchor beacons using an absolute position.
+     * @return The anchor beacon that was added.
+     */
+    private static Beacon addAbsolutePositionAnchorBeacon(String name,
+                                                          double xPosition,
+                                                          double yPosition,
+                                                          String uuid,
+                                                          int major,
+                                                          int minor) {
+
+        Beacon beacon = new Beacon(name, xPosition, yPosition, uuid, major, minor);
+        anchorBeacons.add(beacon);
+        return beacon;
+    }
+
+    private static Beacon addRelativePositionAnchorBeacon(String name,
+                                                          Beacon referenceBeacon,
+                                                          double xPositionOffset,
+                                                          double yPositionOffset,
+                                                          String uuid,
+                                                          int major,
+                                                          int minor) {
+
+        return addAbsolutePositionAnchorBeacon(
+                name,
+                referenceBeacon.getXPosition() + xPositionOffset,
+                referenceBeacon.getYPosition() + yPositionOffset,
+                uuid,
+                major,
+                minor);
+    }
+
+    static {
+        Beacon b1 = addAbsolutePositionAnchorBeacon(
+                "white17 - Entrance",
+                20, 0,
+                "B9407F30-F5F8-466E-AFF9-25556B57FE6D", 46447, 25300);
+        Beacon b2 = addRelativePositionAnchorBeacon(
+                "white5 - Kitchen",
+                b1, -5, 25,
+                "B9407F30-F5F8-466E-AFF9-25556B57FE6D", 33753, 28870);
+    }
 
     private static class Point {
 
@@ -50,8 +100,8 @@ public class Map {
             this.minor = minor;
         }
 
-        public Beacon(String name, double xPosition, double yPosition, UUID uuid, int major, int minor) {
-            this(name, new Point(xPosition, yPosition), uuid, major, minor);
+        public Beacon(String name, double xPosition, double yPosition, String uuid, int major, int minor) {
+            this(name, new Point(xPosition, yPosition), UUID.fromString(uuid), major, minor);
         }
 
         public Point getPosition() {
