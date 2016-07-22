d5deab8a6cb8983565dcf5d55b8bcc51bfff73b5
Bruno Almeida <Bruno@Brunos-MacBook-Air.local>
2016-07-22 15:34:38 -0400

    Refactor ApplicationManager and BeaconRangingService to ApplicationBeaconManager

---
+++ CossetteNavigation/app/src/main/java/com/example/cossettenavigation/ApplicationBeaconManager.java
@@ -0,0 +1,114 @@
+package com.example.cossettenavigation;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+
+import java.util.List;
+
+/**
+ * Global application state used to detect and manage beacons.
+ *
+ * Monitoring is coarse, sending enter and exit events.
+ * Ranging is fine, providing power readings.
+ * - A service to manage beacon ranging (occurs at 1 second intervals).
+ *
+ * Created by Bruno on 2016-07-22.
+ *
+ * @see <a href="http://developer.estimote.com/android/tutorial/part-2-background-monitoring/">Monitoring tutorial</a>
+ * @see <a href="http://developer.estimote.com/android/tutorial/part-3-ranging-beacons/">Ranging tutorial</a>
+ */
+public class ApplicationBeaconManager extends Application {
+
+    private final String TAG = "AppBeaconManager";
+
+    private final Region ALL_BEACONS_REGION = new Region("All Beacons", null, null, null);
+
+    private BeaconManager beaconManager;
+
+
+    @Override
+    public void onCreate() {
+        Log.v(TAG, "onCreate()");
+
+        super.onCreate();
+
+        // Test Map class
+        Map map = new Map();
+
+
+        beaconManager = new BeaconManager(this);
+
+        // Callback when the beacon manager has connected to the beacon service
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                Log.v(TAG, "BeaconManager.ServiceReadyCallback onServiceReady()");
+
+                setMonitoringListener();
+                setRangingListener();
+
+                startMonitoring();
+                startRanging();
+            }
+        });
+    }
+
+
+    private void setMonitoringListener() {
+        beaconManager.setMonitoringListener(new BeaconManager.MonitoringListener() {
+            @Override
+            public void onEnteredRegion(Region region, List<Beacon> list) {
+                Log.v(TAG, "BeaconManager.MonitoringListener onEnteredRegion()");
+
+                Log.v(TAG, "Region: " + region);
+                for (Beacon beacon : list) {
+                    Log.v(TAG, "Beacon: " + beacon);
+                }
+            }
+
+            @Override
+            public void onExitedRegion(Region region) {
+                Log.v(TAG, "BeaconManager.MonitoringListener onExitedRegion()");
+
+                Log.v(TAG, "Region: " + region);
+            }
+        });
+    }
+
+    private void setRangingListener() {
+        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
+            @Override
+            public void onBeaconsDiscovered(Region region, List<Beacon> list) {
+                Log.v(TAG, "BeaconManager.RangingListener onBeaconsDiscovered()");
+
+                Log.v(TAG, "Region: " + region);
+                for (Beacon beacon : list) {
+                    Log.v(TAG, "Beacon: " + beacon);
+                }
+            }
+        });
+    }
+
+
+    private void startMonitoring() {
+        beaconManager.startMonitoring(ALL_BEACONS_REGION);
+    }
+
+    private void stopMonitoring() {
+        beaconManager.stopMonitoring(ALL_BEACONS_REGION);
+    }
+
+
+    private void startRanging() {
+        beaconManager.startRanging(ALL_BEACONS_REGION);
+    }
+
+    private void stopRanging() {
+        beaconManager.stopRanging(ALL_BEACONS_REGION);
+    }
+
+}
