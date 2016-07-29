31f1b37e1e24f0f3e461eb7f23780121afa24493
Gabriel Yeung <gabriel@oriscanada.com>
2016-07-29 11:23:58 -0400

    Embedded search bar into tool bar

--- CossetteNavigation/app/src/main/java/com/example/cossettenavigation/SearchActivity.java
+++ CossetteNavigation/app/src/main/java/com/example/cossettenavigation/SearchActivity.java
@@ -1,11 +1,17 @@
 package com.example.cossettenavigation;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
+import android.widget.Toast;
+
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -16,7 +22,15 @@ public class SearchActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        Intent searchIntent = getIntent();
+
+        if(Intent.ACTION_SEARCH.equals(searchIntent.getAction()))
+        {
+            String query = searchIntent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(SearchActivity.this, query, Toast.LENGTH_SHORT).show();
+        }
+
+        /**FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,7 +38,19 @@ public class SearchActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        return super.onCreateOptionsMenu(menu);
     }
 
 }
