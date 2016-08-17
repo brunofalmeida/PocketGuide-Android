d7a4942cbdbf0069cad8211aabb34e59a15dcce3
jacobjinkelly <jacob.jin.kelly@gmail.com>
2016-08-17 10:45:18 -0400

    added sqlite database
    added SQLiteAssetHelper
    almost ready to implement search suggestions

---
+++ CossetteNavigation/app/src/main/java/com/example/cossettenavigation/map/DatabaseHelper.java
@@ -0,0 +1,19 @@
+package com.example.cossettenavigation.map;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+/**
+ * Created by Jacob on 2016-08-16.
+ */
+public class DatabaseHelper extends SQLiteAssetHelper {
+
+    private static final String DB_NAME="map.db";
+    private static final int DB_VERSION=1;
+
+    public DatabaseHelper(Context context) {
+        super(context,DB_NAME,null,DB_VERSION);
+    }
+}
