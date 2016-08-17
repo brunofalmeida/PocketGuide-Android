08b5ace525a058d3c7c9c8fc28f1f08265c71986
Bruno Almeida <Bruno@brunos-air.identification7.com>
2016-08-17 11:35:08 -0400

    Implement Path class

--- CossetteNavigation/app/src/main/java/com/example/cossettenavigation/pathfinding/Path.java
+++ CossetteNavigation/app/src/main/java/com/example/cossettenavigation/pathfinding/Path.java
@@ -1,7 +1,27 @@
 package com.example.cossettenavigation.pathfinding;
 
+import java.util.ArrayList;
+
 /**
  *
  */
 public class Path {
+
+    private ArrayList<Step> steps;
+    private double travelTime;
+
+
+    public Path(ArrayList<Step> steps, double travelTime) {
+        this.steps = steps;
+        this.travelTime = travelTime;
+    }
+
+    public ArrayList<Step> getSteps() {
+        return steps;
+    }
+
+    public double getTravelTime() {
+        return travelTime;
+    }
+
 }
